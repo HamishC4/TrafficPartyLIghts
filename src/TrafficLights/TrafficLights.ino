5f78fdd8eb45778c608cb6839cb61511be5473ff
HamishC4 <152828887+HamishC4@users.noreply.github.com>
2025-08-06 18:29:41 +1000

    Create TrafficLights.ino

---
+++ src/TrafficLights/TrafficLights.ino
@@ -0,0 +1,21 @@
+/*Software for Traffic Light Controllerf*/
+
+//Authored: Hamish Cranston
+
+//Last updated: 06/08/25
+
+//Version: 1.0
+
+/*Designed to use ESP32 dev board to control array of relays remotely. Relays have the ability to turn on or off individual RGB traffic lights*/
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
